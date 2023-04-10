4e34cc52a4a0d3d6c1e1fa33c2c385f1c60cc23b
inmar salinas <inmarsalinas@yahoo.com>
2023-04-10 14:34:42 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+    let a = 2000;
+    let b = 100;
+    let divide = (a / b)
+    return divide
+}
+
+const square = x => x + 2;
+square;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3);
